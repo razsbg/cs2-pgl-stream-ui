684204c5aec47619e8de76d4bf4a6b52e04802f9
Răzvan Sbîngu <razsbng@gmail.com>
2024-03-23 21:43:23 +0200

    Player component structure

---
+++ src/ui/player-card.jsx
@@ -0,0 +1,47 @@
+import styles from "./player-card.module.css";
+
+export function Player({
+  isSelected,
+  faction,
+  name,
+  isAlive,
+  money,
+  stats: { kills, deaths, hp, adr },
+  inventory: {
+    firearm,
+    sidearm,
+    grenades: { he, flashbang, incendiary, smoke, decoy },
+    defuseKit,
+    armor,
+  },
+}) {
+  return (
+    <div
+      className={`${styles["card"]} ${faction === "ct" ? "ct" : "t"} ${isAlive ? "alive" : ""} ${
+        isSelected ? "selected" : ""
+      }`}
+    >
+      <div className={styles["player"]}>
+        <p className={styles["name"]}>{name}</p>
+        <p className={styles["money"]}>${money}</p>
+      </div>
+      <div className={styles["stats"]}>
+        <p>
+          <span className={styles["kills"]}>{kills}</span>
+          <span className={styles["deaths"]}>{deaths}</span>
+        </p>
+        <p>
+          <span className={styles["defuse-kit"]}>{defuseKit}</span>
+          <span className={styles["smoke"]}>{smoke}</span>
+          <span className={styles["fb"]}>{flashbang}</span>
+          <span className={styles["he"]}>{he}</span>
+        </p>
+      </div>
+      <div className={styles["accent"]}>
+        <span className={styles["hp"]}>{hp}</span>
+        <span className={styles["armor"]}>{armor}</span>
+        <span className={styles["gun"]}>{firearm ?? sidearm}</span>
+      </div>
+    </div>
+  );
+}
