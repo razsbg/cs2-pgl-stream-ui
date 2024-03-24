f1631f7be778c72b3aa04148b5fac2ac7ca3dc89
Răzvan Sbîngu <razsbng@gmail.com>
2024-03-24 17:47:01 +0200

    wip: Player component styles

--- src/ui/player-card.jsx
+++ src/ui/player-card.jsx
@@ -17,29 +17,34 @@ export function Player({
 }) {
   return (
     <div
-      className={`${styles["card"]} ${faction === "ct" ? "ct" : "t"} ${isAlive ? "alive" : ""} ${
+      className={`${styles["player"]} ${faction === "ct" ? "ct" : "t"} ${isAlive ? "alive" : ""} ${
         isSelected ? "selected" : ""
       }`}
     >
-      <div className={styles["player"]}>
-        <p className={styles["name"]}>{name}</p>
-        <p className={styles["money"]}>${money}</p>
-      </div>
-      <div className={styles["stats"]}>
-        <p>
-          <span className={styles["kills"]}>{kills}</span>
-          <span className={styles["deaths"]}>{deaths}</span>
-        </p>
-        <p>
-          <span className={styles["defuse-kit"]}>{defuseKit}</span>
-          <span className={styles["smoke"]}>{smoke}</span>
-          <span className={styles["fb"]}>{flashbang}</span>
-          <span className={styles["he"]}>{he}</span>
-        </p>
+      <div className={styles["container"]}>
+        <div>
+          <p className={styles["name"]}>{name}</p>
+          <p className={styles["money"]}>${money}</p>
+        </div>
+        <div className={styles["stats"]}>
+          <p>
+            <span className={styles["kills"]}>{kills}</span>
+            <span className={styles["deaths"]}>{deaths}</span>
+          </p>
+          <p>
+            <span className={styles["defuse-kit"]}>{defuseKit}</span>
+            <span className={styles["smoke"]}>{smoke}</span>
+            <span className={styles["fb"]}>{flashbang}</span>
+            <span className={styles["he"]}>{he}</span>
+          </p>
+        </div>
       </div>
       <div className={styles["accent"]}>
-        <span className={styles["hp"]}>{hp}</span>
-        <span className={styles["armor"]}>{armor}</span>
+        <div>
+          <span className={styles["hp"]}>{hp}</span>
+          <span className={styles["armor"]}>{armor}</span>
+        </div>
+
         <span className={styles["gun"]}>{firearm ?? sidearm}</span>
       </div>
     </div>
